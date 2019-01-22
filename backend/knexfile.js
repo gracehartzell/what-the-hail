5146914acdd2a35f80bdfee40455e2df8175bb88
Bryan Anderson <bryan.texasbobcat@gmail.com>
2019-01-22 13:23:32 -0600

    server

---
+++ backend/knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgress://localhost/what-the-hail"
+  },
+
+  test: {
+    client: "pg",
+    connection: "postgress://localhost/what-the-hail"
+  }
+};
