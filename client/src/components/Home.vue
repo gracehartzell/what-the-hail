57994d81849acf3531e87deed754f83b11265f72
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-23 16:09:49 -0600

    Cleaned code

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id='app'>
+  <div id="app">
     <Navbar />
     <Login />
     <Register />
@@ -7,20 +7,20 @@
 </template>
 
 <script>
-  import Navbar from "@/components/Navbar";
-  import Login from "@/components/Login";
-  import Register from "@/components/Register";
-  export default {
-    name: "Home",
-    components: {
-      Navbar,
-      Login,
-      Register
-    }
-  };
+import Navbar from "@/components/Navbar";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    Login,
+    Register
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  
+<style>
+
 </style>
