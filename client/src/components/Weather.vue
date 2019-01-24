73103938fa38eb6b118d5111c5b2ac3e253005ad
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-23 18:20:11 -0600

    Cleaned up code

--- client/src/components/Weather.vue
+++ client/src/components/Weather.vue
@@ -5,39 +5,44 @@
         <button type="submit" @click="handleSubmit">Submit</button>
     </div>
 </template>
+
 <script>
 export default {
-    props: ["nextUrl"],
-     data() {
+  props: ["nextUrl"],
+  data() {
     return {
-      city:"",
-      state:""
+      city: "",
+      state: ""
     };
   },
   methods: {
     handleSubmit(e) {
       e.preventDefault();
-     const options = {
+      const options = {
         method: "get",
         headers: {
           Accept: "applications/json",
           "Content-Type": "application/json"
-        },
+        }
       };
 
-      const request = new Request(`https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0JqpVWnvICAal1ryn9I0T7zP99oQCvA4&q=${this.city.charAt(0).toUpperCase()+this.city.substr(1)}&alias=${this.state.charAt(0).toUpperCase()+this.state.substr(1)}`, options);
+      const request = new Request(
+        `https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0JqpVWnvICAal1ryn9I0T7zP99oQCvA4&q=${this.city
+          .charAt(0)
+          .toUpperCase() + this.city.substr(1)}&alias=${this.state
+          .charAt(0)
+          .toUpperCase() + this.state.substr(1)}`,
+        options
+      );
 
       fetch(request)
-      .then((res) =>{
-          return (res.json())
-      })
-      .then((res)=> {
-          console.log(res[0].Key)
-      })
-      
-      
-    
+        .then(res => {
+          return res.json();
+        })
+        .then(res => {
+          console.log(res[0].Key);
+        });
     }
   }
-}
+};
 </script>
