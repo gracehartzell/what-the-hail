96c7ee107dfe37c69b758b75c6e0453e24c91b6d
Mitali Mittal <mits254@gmail.com>
2019-01-23 18:40:19 -0600

    Enter

--- client/src/components/Weather.vue
+++ client/src/components/Weather.vue
@@ -1,44 +1,48 @@
 <template>
-    <div>
-        <input id="city" type="text" v-model="city" required>
-        <input id="state" type="text" v-model="state" required>
-        <button type="submit" @click="handleSubmit">Submit</button>
-        <h4>{{res[0].LocalizedName}}</h4>
-    </div>
+  <div>
+    <input id="city" type="text" v-model="city" required>
+    <input id="state" type="text" v-model="state" required>
+    <button type="submit" @click="handleSubmit">Submit</button>
+    <h4>{{res[0].LocalizedName}}</h4>
+  </div>
 </template>
 <script>
 export default {
-    props: ["nextUrl"],
-     data() {
+  props: ["nextUrl"],
+  data() {
     return {
-      city:"",
-      state:""
+      city: "",
+      state: ""
     };
   },
   methods: {
     handleSubmit(e) {
       e.preventDefault();
-     const options = {
+      const options = {
         method: "get",
         headers: {
           Accept: "applications/json",
           "Content-Type": "application/json"
-        },
+        }
       };
 
-      const request = new Request(`https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/search?apikey=3G82zFHhr6JnoMvvrcfscDsmO6bSzlWg&q=${this.city.charAt(0).toUpperCase()+this.city.substr(1)}&alias=${this.state.charAt(0).toUpperCase()+this.state.substr(1)}`, options);
+      const request = new Request(
+        `https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/search?apikey=3G82zFHhr6JnoMvvrcfscDsmO6bSzlWg&q=${this.city
+          .charAt(0)
+          .toUpperCase() + this.city.substr(1)}&alias=${this.state
+          .charAt(0)
+          .toUpperCase() + this.state.substr(1)}`,
+        options
+      );
 
       fetch(request)
-      .then((res) =>{
-          return (res.json())
-      })
-      .then((res)=> {
-          console.log(res[0].LocalizedName)
-      })
-      
-      
-    
+        .then(res => {
+          return res.json();
+        })
+        .then(res => {
+          console.log(res[0].LocalizedName);
+        });
     }
   }
-}
+};
 </script>
